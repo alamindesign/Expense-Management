1ea85796a34d9f682042ee92a5f427333d645fe5
alamindesign <mail.muhammadit@gmail.com>
2023-02-03 22:50:30 +0600

    creating user

--- Expense_Management/Expense_Management/settings.py
+++ Expense_Management/Expense_Management/settings.py
@@ -136,13 +136,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #jazzmin
 JAZZMIN_SETTINGS = {
     # # title of the window (Will default to current_admin_site.site_title if absent or None)
-    # "site_title": "Library Admin",
+    "site_title": "Expense Management",
 
     # # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    # "site_header": "Library",
+    "site_header": "Expense Mangement",
 
     # # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
-    # "site_brand": "Library",
+    "site_brand": "Expense Management",
 
     # # Logo to use for your site, must be present in static files, used for brand on top left
     # "site_logo": "books/img/logo.png",
@@ -160,10 +160,10 @@ JAZZMIN_SETTINGS = {
     # "site_icon": None,
 
     # # Welcome text on the login screen
-    # "welcome_sign": "Welcome to the library",
+    "welcome_sign": "Welcome to the Expense Mangement System",
 
     # # Copyright on the footer
-    # "copyright": "Acme Library Ltd",
+    "copyright": "DWD-Group 2",
 
     # # List of model admins to search from the search bar, search bar omitted if excluded
     # # If you want to use a single search field you dont need to use a list, you can use a simple string 
