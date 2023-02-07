707f2dd54f8392696008426e0cecfe3da182c12b
alamindesign <mail.muhammadit@gmail.com>
2023-02-08 01:03:32 +0600

    using index as home

--- ExpenseManagement/ExpenseApp/views.py
+++ ExpenseManagement/ExpenseApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def Home(request):
-    return render(request,'expenseApp/base.html')
+    return render(request,'expenseApp/index.html')
