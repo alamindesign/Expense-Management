6b0676d061706a7ffc32d80983fef1574ce76d9e
alamindesign <mail.muhammadit@gmail.com>
2023-02-08 15:35:21 +0600

    course

--- ExpenseManagement/ExpenseApp/views.py
+++ ExpenseManagement/ExpenseApp/views.py
@@ -4,3 +4,6 @@ from .models import *
 def Home(request):
     all_expense = DailyExpense.objects.all()
     return render(request,'expenseApp/index.html',{'expense': all_expense})
+def Courses(request):
+    course = Student.objects.all()
+    return render(request,'expenseApp/course.html',{'course':course})
