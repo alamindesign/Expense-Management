ea1b07eef93f19a13fd5ac0be008ef3966bc57f4
ashimghoshfm <ashimghoshfm@gmail.com>
2023-02-09 09:12:52 +0600

    ok, menubar icon's added

--- ExpenseManagement/ExpenseManagement/settings.py
+++ ExpenseManagement/ExpenseManagement/settings.py
@@ -143,19 +143,19 @@ JAZZMIN_SETTINGS = {
     "site_brand": "Expense Management",
 
     # # Logo to use for your site, must be present in static files, used for brand on top left
-    # "site_logo": "books/img/logo.png",
+    "site_logo": "images/icon.png",
 
     # # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
-    # "login_logo": None,
+    "login_logo": "images/icon.png",
 
     # # Logo to use for login form in dark themes (defaults to login_logo)
     # "login_logo_dark": None,
 
     # # CSS classes that are applied to the logo above
-    # "site_logo_classes": "img-circle",
+    "site_logo_classes": "images/icon.png",
 
     # # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    # "site_icon": None,
+    "site_icon": "images/icon.png",
 
     # # Welcome text on the login screen
     "welcome_sign": "Welcome to the Expense Mangement System",
