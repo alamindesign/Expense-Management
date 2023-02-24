488681463e35aec09b31db90ee7b5c49d250bb02
ashimghoshfm <ashimghoshfm@gmail.com>
2023-02-25 00:41:20 +0600

    migration error fixed

--- ExpenseManagement/ExpenseManagement/settings.py
+++ ExpenseManagement/ExpenseManagement/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ExpenseApp',
-    'django.core.paginator', #for pagination
 ]
 
 MIDDLEWARE = [
